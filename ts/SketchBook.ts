f95e8133af2a55e6d4e35fc29ffc132cc4783439
Jancik, Tomas <tjancik@netsuite.com>
2019-12-18 15:22:02 +0100

    add new Sketch

--- ts/SketchBook.ts
+++ ts/SketchBook.ts
@@ -1,12 +1,47 @@
+interface Sketch {
+    name: string;
+    text?: string;
+}
+
 class SketchBook {
     private static addEl: Element = document.querySelector("#sketchAdd") as Element;
+    private sketches: Sketch[] = [];
+
+    private addSketchElement(sketch: Sketch): void {
+        const el: Element = document.createElement(this.tagname);
+        el.textContent = sketch.name;
+
+        this.target.appendChild(el);
+    }
 
     private handleAddClicked = (e: Event): void => {
-        console.log("add clicked");
+        this.sketches.push({
+            name: "New Sketch " + this.sketches.length + 1,
+        });
+
+        this.render();
+        this.editSketch(this.sketches.length - 1);
+    }
+
+    private removeAllSketchElement(): void {
+        document.querySelectorAll(this.tagname + ".sketch").forEach((e: Element) => {
+            e.remove();
+        });
     }
 
     public constructor(private target: Element, private navigation: Element, private tagname: string = "li") {
         SketchBook.addEl.addEventListener("click", this.handleAddClicked);
+    }
+
+    public editSketch(id: number):void {
+        console.log(id, this.sketches[id]);
+    }
+
+    public render(): void {
+        this.removeAllSketchElement();
+        this.sketches.forEach((sketch: Sketch) => {
+            this.addSketchElement(sketch);
+        });
     }
 }
 
